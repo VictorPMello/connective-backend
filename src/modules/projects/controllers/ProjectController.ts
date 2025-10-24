3aaab031c9492eaffd63cf20642620ecdad6ccc7
Victor Pinheiro Mello <victormello7003@hotmail.com>
2025-10-24 16:58:16 -0300

    feat: Project Controller

---
+++ src/modules/projects/controllers/ProjectController.ts
@@ -0,0 +1,104 @@
+import type { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+
+import { CreateProjectService } from "../services/CreateProjectService.ts";
+import { GetProjectService } from "../services/GetProjectService.ts";
+import { UpdateProjectService } from "../services/UpdateProjectService.ts";
+import { DeleteProjectService } from "../services/DeleteProjectService.ts";
+
+export class ProjectController {
+  private createProjectService: CreateProjectService;
+  private getProjectService: GetProjectService;
+  private updateProjectService: UpdateProjectService;
+  private deleteProjectService: DeleteProjectService;
+
+  constructor(
+    createProjectService: CreateProjectService,
+    getProjectService: GetProjectService,
+    updateProjectService: UpdateProjectService,
+    deleteProjectService: DeleteProjectService,
+  ) {
+    this.createProjectService = createProjectService;
+    this.getProjectService = getProjectService;
+    this.updateProjectService = updateProjectService;
+    this.deleteProjectService = deleteProjectService;
+  }
+
+  // CREATE
+  async create(request: FastifyRequest, reply: FastifyReply) {
+    const createProjectSchema = z.object({
+      title: z.string(),
+      description: z.string(),
+      accountId: z.string(),
+    });
+
+    const data = createProjectSchema.parse(request.body);
+    const newProject = await this.createProjectService.createProject(data);
+
+    return reply.status(201).send({
+      success: true,
+      message: "Project created successfully",
+      data: newProject,
+    });
+  }
+
+  // GET
+
+  async getById(request: FastifyRequest, reply: FastifyReply) {
+    const paramsSchema = z.object({ id: z.string().uuid() });
+    const { id } = paramsSchema.parse(request.params);
+
+    const project = await this.getProjectService.getProjectById(id);
+
+    if (!project) {
+      return reply.status(404).send({
+        success: false,
+        message: "Project not found",
+      });
+    }
+
+    return reply.status(200).send({
+      success: true,
+      data: project,
+    });
+  }
+
+  // UPDATE
+
+  async update(request: FastifyRequest, reply: FastifyReply) {
+    const paramsSchema = z.object({ id: z.string().uuid() });
+    const { id } = paramsSchema.parse(request.params);
+
+    const updateProjectSchema = z.object({
+      title: z.string(),
+      description: z.string(),
+    });
+
+    const data = updateProjectSchema.parse(request.body);
+
+    const project = await this.updateProjectService.updateProject(id, {
+      ...data,
+      updatedAt: new Date(),
+    });
+
+    return reply.status(200).send({
+      success: true,
+      message: "Task updated successfully",
+      data: project,
+    });
+  }
+
+  // DELETE
+
+  async delete(request: FastifyRequest, reply: FastifyReply) {
+    const paramsSchema = z.object({ id: z.string().uuid() });
+    const { id } = paramsSchema.parse(request.params);
+
+    await this.deleteProjectService.deleteProject(id);
+
+    return reply.status(200).send({
+      success: true,
+      message: "Project deleted successfully",
+    });
+  }
+}
